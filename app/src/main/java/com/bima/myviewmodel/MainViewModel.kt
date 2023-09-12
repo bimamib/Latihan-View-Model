31327a41a51e13a432b7540370fe91b9375b53cf
bimamib <bimaprast1601@gmail.com>
2023-09-12 21:42:08 +0700

    menambahkan code pada MainViewModel

--- app/src/main/java/com/bima/myviewmodel/MainViewModel.kt
+++ app/src/main/java/com/bima/myviewmodel/MainViewModel.kt
@@ -1,4 +1,11 @@
 package com.bima.myviewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var result = 0
+
+    fun calculate(width: String, height: String, length: String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
 }
